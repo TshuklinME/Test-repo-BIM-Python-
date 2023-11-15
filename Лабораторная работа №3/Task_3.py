4ed69a5cea09ef6236d72c8fbadb6a91b7ade289
TshuklinME <149962556+TshuklinME@users.noreply.github.com>
2023-11-15 11:57:13 +0300

    Update Task_3.py

--- Лабораторная работа №3/Task_3.py
+++ Лабораторная работа №3/Task_3.py
@@ -1,16 +1,12 @@
 # TODO  Напишите функцию count_letters
 def count_letters (text):
-    lower_text = text.lower() #Приводим все к нижнему регистру
-    src_text = ''.join(lower_text.split())  #Удаляем все пробелы
-    modified_text = src_text.replace('\n','') #Удаляем все переносы строк
+    modified_text = text.lower() #Приводим все к нижнему регистру
 
-    marks = "!()-—[]{};?@#$%:,.^&8*…"
     dict_ = {}
-    count = 0
 
     for i in modified_text:
-        if i not in marks:
-            dict_[i] = dict_.get(i,count)+1
+        if i.isalpha():
+            dict_[i] = dict_.get(i,0)+1
 
     return dict_
 # TODO Напишите функцию calculate_frequency
@@ -18,9 +14,8 @@ def calculate_frequency (dict_):
     total = sum(dict_.values()) #Вычисляется сумма
 
     for i in dict_:
-        dict_[i] = round(dict_[i]/total,2)
-        if dict_[i] == 0.0: #добавил для того, что в ответе отображалось 0.00 вместо 0.0
-            dict_[i] = "0.00"
+        rounding_value = round(dict_[i]/total,2)
+        dict_[i] = f"{rounding_value:.2f}"
 
     return dict_
 
